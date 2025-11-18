3a6aecaebd6295325992f791cdf9b75e92a5f7d8
Alexandre Cesar <alexandrecesar.dev@gmail.com>
2025-11-17 21:26:45 -0300

    chore: remove vite.config.js duplicado e configura base correta no vite.config.ts para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/alexandre-portfolio/',
-  plugins: [vue()]
+  plugins: [vue()],
+  base: '/alexandre-portfolio/'
 })
