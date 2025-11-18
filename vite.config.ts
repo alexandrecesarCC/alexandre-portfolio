a4946cfb2b6cf344de0a74d3da1aa574ec102f14
Alexandre Cesar <alexandrecesar.dev@gmail.com>
2025-11-18 01:20:54 -0300

    Atualização: Configuração do GitHub Pages + Vite base

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
+  base: "/alexandre-portfolio/",
+
   plugins: [vue()],
-  base: '/alexandre-portfolio/'
+  resolve: {
+    alias: {
+      'vue': 'vue/dist/vue.esm-bundler.js',
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
